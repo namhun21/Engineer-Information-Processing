8f6d017abb58d080a3209e4b5f25a1895c628d6a
namhun21 <namhun21@naver.com>
2019-12-31 00:15:36 +0900

    section30 해야함

--- Engineer-Information-Processing/src/basic_Algorithm_math/section28.java
+++ Engineer-Information-Processing/src/basic_Algorithm_math/section28.java
@@ -1,10 +1,46 @@
 package basic_Algorithm_math;
-
-public class section28 {
+import java.util.*;
+public class section28 { //1의 보수 , 2의 보수
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		int[] num = new int[5];
+		for(int i = 0; i< 5 ; i++) {
+			System.out.print("num["+i+"] : ");
+			num[i] = stdIn.nextInt();
+		}
 
+		getTwoBosu(num);
+
+	}
+	static void getOneBosu(int[] num) {
+		for(int i = 0 ; i< num.length ; i++)
+			num[i] = 1 - num[i];
+		for(int i: num)
+			System.out.print(i);
+		System.out.println();
+	}
+	static void getTwoBosu(int[] num) {
+		int carry = 0;
+		for(int i = 0 ; i< num.length ; i++)
+			num[i] = 1 - num[i];
+		num[4]++;
+		for(int j = num.length -1 ; j>0 ; j--) {
+			if(num[j] == 2) {
+				num[j] = 0;
+				carry = 1;
+				num[j-1] += carry;
+			}else 
+				carry = 0;
+		
+		}
+		if(num[0] == 2)
+			System.out.println("OverFlow");
+		else {
+			for(int i = 0 ; i< num.length ; i++)
+				System.out.print(num[i]);
+		}
+		System.out.println();
 	}
 
 }
---
+++ Engineer-Information-Processing/src/basic_Algorithm_math/section30.java
@@ -0,0 +1,10 @@
+package basic_Algorithm_math;
+
+public class section30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
